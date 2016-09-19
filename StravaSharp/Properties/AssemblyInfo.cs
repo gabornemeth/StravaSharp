10ab2115dd577926d87d969817ada16946ea2ffe
Németh Gábor <gabor.nemeth1982@gmail.com>
2016-09-19 14:36:18 +0200

    update nuspec, make available as nuget package

--- StravaSharp/Properties/AssemblyInfo.cs
+++ StravaSharp/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("Portable .NET library for Strava")]
+[assembly: AssemblyDescription("Strava client library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © Gabor Nemeth 2015")]
+[assembly: AssemblyCopyright("Copyright © Gabor Nemeth 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.0")]
